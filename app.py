ecf8303890a33226057b91b73d8fd0e8771d3828
flutra <flutramusaj0@gmail.com>
2024-12-20 15:10:32 +0100

    DAO Pattern

--- app.py
+++ app.py
@@ -2,19 +2,14 @@ import psycopg2
 import secrets
 from flask import Flask, render_template, request, redirect, url_for, session
 from passlib.hash import sha1_crypt
+from components.userDAO import UserDao
 from messages import LOGIN_SUCCESS, LOGIN_FAIL, REGISTER_SUCCESS, REGISTER_FAIL
 from components.user import User
 from flask import flash
 from components.feedback import Feedback
-from components.presentation_manager import PresentationManager
-from components.mode import Mode
-
 
 app = Flask(__name__)
 app.secret_key = secrets.token_hex(16)
-presentation_manager = PresentationManager()
-
-presentation_manager.start()
 
 @app.route('/')
 def home():
@@ -62,14 +57,16 @@ def db_conn():
 def createUser():
     username = request.form['username']
     email = request.form['email']
-    password = request.form['password']
+    password = sha1_crypt.hash(request.form['password'])
     level = request.form['security_level']
     user = User(username, email, password, level)
 
-    if user.add_user():
+    user_dao = UserDao()
+
+    if user_dao.add_user(user):
         flash(REGISTER_SUCCESS, 'success')
         return render_template('login.html')
-   
+
     flash(REGISTER_FAIL, 'error')
     return render_template('register.html')
 
@@ -100,7 +97,8 @@ def login_user():
             session['user_email'] = email
             flash(LOGIN_SUCCESS, 'success')
 
-            return render_template('index.html')
+            # Redirect to the homepage or any desired route after successful login
+            return redirect(url_for('index'))
        
         flash(LOGIN_FAIL, 'error')
         # If login fails, you can render an error message or redirect back to the login page
@@ -142,6 +140,30 @@ def get_level_by_email(email):
     if level:
         return level
 
+# @app.route('/update_email', methods=['POST'])
+# def update_email():
+#     if 'user_email' in session:
+#         current_email = session['user_email']
+#         new_email = request.form['new_email']
+
+#         if not new_email:
+#             flash('New email cannot be empty.', 'error')
+#             return redirect(url_for('profile'))
+
+#         # Create an instance of the User class
+#         user = User(username=get_username_by_email(current_email), email=current_email, password=get_username_by_email(current_email), level=get_level_by_email(current_email))
+   
+#         # Call the updateEmail method
+#         if user.updateEmail(new_email):
+#             flash('Email updated successfully.', 'success')
+#         else:
+#             flash('Error updating email.', 'error')
+
+#         return redirect(url_for('profile'))
+
+#     flash('Please log in to access your profile.', 'error')
+#     return render_template('profile.html')
+
 @app.route('/update_email', methods=['POST'])
 def update_email():
     if 'user_email' in session:
@@ -152,11 +174,10 @@ def update_email():
             flash('New email cannot be empty.', 'error')
             return redirect(url_for('profile'))
 
-        # Create an instance of the User class
-        user = User(username=get_username_by_email(current_email), email=current_email, password=get_username_by_email(current_email), level=get_level_by_email(current_email))
-   
-        # Call the updateEmail method
-        if user.updateEmail(new_email):
+        # Create an instance of UserDao
+        user_dao = UserDao()
+        # Call the update_email method
+        if user_dao.update_email(User(email=current_email), new_email):
             flash('Email updated successfully.', 'success')
         else:
             flash('Error updating email.', 'error')
@@ -177,15 +198,14 @@ def update_password():
             return redirect(url_for('profile'))
 
         # Create an instance of the User class
-        user = User(username=get_username_by_email(current_email), email=current_email, password=get_username_by_email(current_email), level=get_level_by_email(current_email))
-   
+        user_dao = UserDao()    
         # Call the updateEmail method
-        if user.resetPassword(new_password):
+        if user_dao.reset_password(User(email=current_email), new_password):
             flash('Password updated successfully.', 'success')
         else:
             flash('Error updating password.', 'error')
 
-        return redirect(url_for('profile'))
+        return redirect(url_for('login'))
 
     flash('Please log in to access your profile.', 'error')
     return render_template('profile.html')
@@ -200,10 +220,9 @@ def update_level():
             flash('New level cannot be empty.', 'error')
             return redirect(url_for('profile'))
 
-        # Create an instance of the User class
-        user = User(username=get_username_by_email(current_email), email=current_email, password=get_username_by_email(current_email), level=get_level_by_email(current_email))
-   
-        if user.updateSecurityLevel(new_level):
+        user_dao = UserDao()
+
+        if user_dao.update_security_level(User(email=current_email), new_level):
             flash('Level updated successfully.', 'success')
         else:
             flash('Error updating level.', 'error')
@@ -218,12 +237,9 @@ def delete_account():
     if 'user_email' in session:
         current_email = session['user_email']
        
-        # Create an instance of the User class
-        user = User(username=get_username_by_email(current_email), email=current_email, password=get_username_by_email(current_email), level=get_level_by_email(current_email))
+        user_dao = UserDao()    
 
-        # Call the deleteAccount method
-        if user.deleteAccount():
-            # Clear the user session data after account deletion
+        if user_dao.delete_account(User(email=current_email)):
             session.pop('user_email', None)
             flash('Account deleted successfully.', 'success')
             return redirect(url_for('home'))
@@ -305,5 +321,4 @@ def index():
     return render_template('users.html', data = data)
  
 if __name__ == '__main__':
-    presentation_manager.modeManager()
     app.run(debug=True)
